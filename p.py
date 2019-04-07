6b1b73dee5a32456580be59854e2332fea2b4371
KevinSk <kevi0023@e.ntu.edu.sg>
2019-04-07 13:43:12 +0800

    Add AllGuesses and AllOptions buttons

--- p.py
+++ p.py
@@ -11,25 +11,34 @@ def start():
     #init input listener
     x = StringVar()
     y = StringVar()
+    counter = StringVar()
+    counter.set(0)
     category = StringVar()
     category.set("OLYMPIC SPORTS")
     #add choice to category list
     choices=["OLYMPIC SPORTS","POKEMON"]
-
-
+    queries_done = []
+    
     #random selection of sports
     #query for selection to store in python
-    selected = "tennis"
-
+    selected_guess = list(prolog.query("selected(X)"))[0]['X']
+    print(selected_guess)
 
     def handleQuery(event=None):
         if(x.get()!=""):
-            # get number of queries left
-            counter = list(prolog.query("counter(X)",1))[0]['X']
-            if (counter < 10):
+            if(x.get().lower() in queries_done):
+                messagebox.showwarning("Queries done!","has("+x.get().lower()+") was queried!")
+            elif (int(counter.get()) < 9):
                 # format query
                 qry = "has("+x.get().lower()+")"
                 q_result = prolog.query(qry)
+
+                # get counter to see number of queries left
+                counter.set(list(prolog.query("counter(X)"))[0]['X'])
+                print(list(prolog.query("counter(X)")))
+                label_counter.config(text="Number of Guesses: "+str(9-int(counter.get())))
+                #keep track of queries done to remove duplicate query
+                queries_done.append(x.get().lower())
                 # true if return {}, false if nothing
                 if(list(q_result) == []):
                     #insert to false listbox
@@ -50,28 +59,62 @@ def start():
         x.set("")
 
     def change_dropdown(*args):
+        # every change of category pick random item
+        # prolog retract and assert category picked
         print( category.get() )
 
-    def handleSubmit():
+    def handleGuess():
         if(y.get()!= ""):
             qry = "is("+y.get().lower()+")"
             q_result = prolog.query(qry)
             # true if return {}, false if nothing
             if(list(q_result) == []):
                 #WRONG GUESS, give answer, restart game
-                messagebox.showinfo("RESULT","You Got It WRONG!")
+                messagebox.showinfo("RESULT","You Got It WRONG! Correct answer is "+selected_guess)
                 
             else:
                 #CORRECT GUESS
                 messagebox.showinfo("RESULT","You Got It RIGHT! Congrats")
+            restart_game(root)
         y.set("")
-        restart_game(root)
-
         
-    # link function to change dropdown
+
+    def handleAllQuery():
+        #create a new small window to display all available query
+        list_query = Toplevel()
+        list_query.title("List of All Queries")
+        query_listbox = Listbox(list_query)
+        query_listbox.config(height=0,width=0)
+        #generate list of items
+        for each_q in list(prolog.query("flat(X)"))[0]['X']:
+            query_listbox.insert(END,"has("+str(each_q)+")")
+        query_listbox.pack(fill=BOTH,expand=1,ipadx=80)
+        #bind user select on listbox to event listener
+        query_listbox.bind('<<ListboxSelect>>',curSelec)
+    
+    # listbox click event listener
+    # update has(X) entry by user click
+    def curSelec(evt):
+        active_item = evt.widget.get(evt.widget.curselection())
+        x.set("".join(list(active_item)[4:-1]))        
+    
+    def handleAllGuess():
+        #create a new small window to display all available query
+        list_guess = Toplevel()
+        list_guess.title("List of All Guesses")
+        guess_listbox = Listbox(list_guess)
+        guess_listbox.config(height=0,width=0)
+        #generate list of items
+        if(category.get() == "OLYMPIC SPORTS"):
+            for each_g in list(prolog.query("randomizer_sports(X)"))[0]['X']:
+                guess_listbox.insert(END,"is("+str(each_g)+")")
+            guess_listbox.pack(fill=BOTH,expand=1,ipadx=80)
+        
+
+    # link function to change dropdown as selected
     category.trace('w', change_dropdown)
 
-    #bind enter to Query button
+    #bind enter-key to Query button
     root.bind('<Return>', handleQuery)
 
     label_title = Label(root, text="10 Questions",font=("Roboto",30))
@@ -84,14 +127,16 @@ def start():
     button_q = Button(mid_frame,text="Query", command=handleQuery,width=5)
     label_is = Label(mid_frame, text = "is(")
     label_is2 = Label(mid_frame, text = ")?")
-    submit = Entry(mid_frame,textvariable=y)
-    button_is = Button(mid_frame,text="Submit",command=handleSubmit,width=5)
+    guess = Entry(mid_frame,textvariable=y)
+    button_guess = Button(mid_frame,text="Guess",command=handleGuess,width=5)
     history_frame = LabelFrame(root,bd=5,relief=GROOVE,text="Queries")
+    label_counter = Label(history_frame,text="Number of Guesses: 10")
     hist_listbox_t = Listbox(history_frame,activestyle=NONE,yscrollcommand=Scrollbar(history_frame,orient=VERTICAL).set)
     label_t = Label(history_frame, text = "True")
     hist_listbox_f = Listbox(history_frame,activestyle=NONE,yscrollcommand=Scrollbar(history_frame,orient=VERTICAL).set)
     label_f = Label(history_frame, text = "False")
-
+    button_all_query = Button(history_frame,text="All Queries",command=handleAllQuery,width=5)
+    button_all_guess = Button(history_frame,text="Guesses",command=handleAllGuess,width=5)
     label_title.grid(row=0,columnspan=5,sticky= N+E+W+S)
 
     label_category.grid(row=1,columnspan=5,sticky=S)
@@ -105,20 +150,25 @@ def start():
     label_has2.grid(row=0,column=2,sticky=W)
     button_q.grid(row=0,column=4,sticky=E,padx=(20,0))
     label_is.grid(row=1,sticky=E,padx=(20,0))
-    submit.grid(row=1,column=1)
+    guess.grid(row=1,column=1)
     label_is2.grid(row=1,column=2,sticky=W)
-    button_is.grid(row=1,column=4,sticky=E,padx=(20,0))
+    button_guess.grid(row=1,column=4,sticky=E,padx=(20,0))
     #hist Frame
     history_frame.grid(row=4, columnspan=5,sticky= N+E+W+S)
-    label_t.grid(row=0)
-    label_f.grid(row=0,column=1)
-    hist_listbox_t.grid(row=1)
-    hist_listbox_f.grid(row=1,column=1)
+    label_counter.grid(row=0,columnspan=2)
+    label_t.grid(row=1)
+    label_f.grid(row=1,column=1)
+    hist_listbox_t.grid(row=2)
+    hist_listbox_f.grid(row=2,column=1)
+
+    button_all_query.grid(row=3,sticky= N+E+W+S)
+    button_all_guess.grid(row=3,column=1,sticky= N+E+W+S)
     root.mainloop()
 
 
 def restart_game(root):
-    root.destro()
+    messagebox.showinfo("Restart","Game restarting!")
+    root.destroy()
     start()
 
 start()
--- q.pl
+++ q.pl
@@ -3,45 +3,96 @@
 /* team_size, arena_type, play_device, game_mode, winning_condition */
 tennis([court,single,doubles,outdoor,ball,racket,highestpoint]).
 swimming([pool,single,water,fastest]).
-basketball([team,court,indoor,ball,highestpoint,fiveman]).
+basketball([team,court,indoor,ball,highestpoint,five_man]).
 table_tennis([single,doubles,table,ball,racket,highestpoint,indoor]).
-snooker([table,single,ball,stick,highestpoint]).
-waterpolo([team,sevenman,indoor,water,ball,highestpoint,pool]).
+waterpolo([team,seven_man,indoor,water,ball,highestpoint,pool]).
 track([single,outdoor,fastest,court]).
-floorball([court,team,sixman,ball,stick,indoor,highestpoint]).
+floorball([court,team,six_man,ball,stick,indoor,highestpoint]).
+
+randomizer_sports([tennis,swimming,basketball,table_tennis,waterpolo,track,floorball]).
 
 /* RULES */
 has(X):-
     tennis(L),
     selected(tennis),
-    member(X,L),
-    incr.
+    incr,
+    member(X,L).
 
 has(X):-
     swimming(L),
     selected(swimming),
-    member(X,L),
-    incr.
+    incr,
+    member(X,L).
 
 has(X):-
     basketball(L),
     selected(basketball),
-    member(X,L),
-    incr.
+    incr,
+    member(X,L).
 
 has(X):-
     table_tennis(L),
     selected(table_tennis),
-    member(X,L),
-    incr.
+    incr,
+    member(X,L).
+
+has(X):-
+    waterpolo(L),
+    selected(waterpolo),
+    incr,
+    member(X,L).
+
+has(X):-
+    track(L),
+    selected(track),
+    incr,
+    member(X,L).
+
+has(X):-
+    floorball(L),
+    selected(floorball),
+    incr,
+    member(X,L).
 
 
 is(X):-
     selected(X).
 
 
+pick_random :-
+    category(sports),
+    randomizer_sports(L),
+    random_member(X,L),
+    retractall(selected(_)),
+    assertz(selected(X)).
 
-:- dynamic counter/1.
+list_flat :-
+    tennis(A),
+    swimming(B),
+    append(A,B,L),
+    basketball(C),
+    append(L,C,L2),
+    table_tennis(D),
+    append(L2,D,L3),
+    waterpolo(E),
+    append(L3,E,L4),
+    track(F),
+    append(L4,F,L5),
+    floorball(G),
+    append(L5,G,L6),
+    flatten(L6,L7),
+    remove_dupe(L7,L8),
+    retractall(flat(_)),
+    assertz(flat(L8)).
+
+/* removing duplicate from flatten list */
+remove_dupe([],L). 
+remove_dupe([H|T],L) :-
+    member(H,L) ->
+    remove_dupe(T,L);
+    append(L,[H],L2),
+    remove_dupe(T,L2).
+
 
 initcounter :-
     retractall(counter(_)),
@@ -53,10 +104,13 @@ incr :-
     succ(V0, V),
     assertz(counter(V)).
 
+/* init functions */
+category(sports).
 :- initcounter.
 
+:- pick_random.
 
+:- list_flat.
 
-selected(tennis).
 
 
